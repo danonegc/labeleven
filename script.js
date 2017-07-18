aadd779a89012c38d09c64bba3a346442186c1e1
danonegc <dcureton@gmail.com>
2017-07-18 07:58:19 -0400

    Initial submission

---
+++ script.js
@@ -0,0 +1,14 @@
+$.get('https://www.reddit.com/r/awww.json').done(function(json){
+    json.data.children.forEach.(function(json){
+      console.log(json.data.title);
+
+      var title = json.data.title;
+      var thumbnail = json.data.thumbnail;
+      var author = json.data.author; {
+      $('#images').append("<div .container ><p>" + 'Title: ' + title +
+      '</br>' + 'Author: ' + author + '</p><img src=' + thumbnail + '></div>');
+    };
+  });
+$('#images div:gt(15)').remove();
+
+ });
